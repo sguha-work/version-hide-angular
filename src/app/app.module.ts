c5e7458d944b605c3a342a3a7f86274dc027df88
Guha <sahguha@apac.corpdir.net>
2023-08-11 12:35:48 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+declare let document: any, window: any;
 @NgModule({
   declarations: [
     AppComponent
@@ -13,4 +13,13 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    const getComponentTimer = window.setInterval(()=>{
+      if(document.getElementsByTagName('app-root').length) {
+        window.clearInterval(getComponentTimer);
+        document.getElementsByTagName('app-root')[0].removeAttribute('ng-version');
+      }
+    });
+  }
+}
